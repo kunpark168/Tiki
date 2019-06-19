f07dfbda73f791dce97abdbe5dba2770f20cc3ed
Do Anh Tam <pikakun19@gmail.com>
2019-06-19 10:00:01 +0700

    Location and check box (UI)

---
+++ app/src/main/java/com/anhtam/tikihome/BaseActivity.java
@@ -0,0 +1,22 @@
+package com.anhtam.tikihome;
+
+import android.content.res.Resources;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base);
+    }
+
+}
--- app/src/main/java/com/anhtam/tikihome/MainActivity.java
+++ app/src/main/java/com/anhtam/tikihome/MainActivity.java
@@ -1,14 +1,45 @@
 package com.anhtam.tikihome;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ConstraintLayout csCheckBox;
+    private boolean isTikiNow = true;
+    private ImageView imgCheckBox;
+    private TextView tvLocation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView ();
+        initData ();
+        initControls ();
+    }
+
+    private void initView (){
+        csCheckBox = findViewById(R.id.csCheckBox);
+        imgCheckBox = findViewById(R.id.imgCheckBox);
+        tvLocation = findViewById(R.id.tvLocation);
+    }
+
+    private void initData (){
+        String htmlString="<u>" + getResources().getString(R.string.address_example)+ "</u>";
+        tvLocation.setText(Html.fromHtml(htmlString));
+    }
+
+    private void initControls (){
+        csCheckBox.setOnClickListener(v -> onCheckTikiNow ());
+    }
+
+    private void onCheckTikiNow (){
+        isTikiNow = !isTikiNow;
+        imgCheckBox.setImageDrawable(isTikiNow ? getResources().getDrawable(R.drawable.ic_tick) : getResources().getDrawable(R.drawable.ic_untick));
     }
 }
